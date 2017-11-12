08506eaffba31131402bb4e79cf6a66226967b2d
bodaay <Khlaefa@addrek.com>
2017-11-12 23:16:52 +0300

    reached lecture 30

---
+++ src/components/video_list.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import VideoListItem from './video_list_item'
+const VideoList = (props) =>{
+    const videoitems = props.videos.map( (video) => {
+        return (
+            <VideoListItem 
+            onVideoSelect={props.onVideoSelect}
+            key={video.etag} video={ video } />
+        );
+    });
+    const videos =  props.videos;
+    return (
+        <ul className="col-md-4 listgroup">
+        {videoitems}
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyBF0ZO7Ut3T_GqemzqZmbBiGLj5vw9_Lbk';
 
 
-YTSearch({key:API_KEY,term: 'surfboards'},function(data){
-    console.log(data);
-})
 
-const App = () => {
-    return (
-    <div>
-    Hi!
-    <SearchBar />
-    </div>
-    );
+
+
+class App extends Component{
+    constructor (props){
+        super(props);
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+        YTSearch({key:API_KEY,term: 'surfboards'},(videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            }); //we can as well write it as: this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+            Hi!
+            <SearchBar />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList 
+            onVideoSelect={ (selectedVideo) => this.setState({selectedVideo})}
+            videos={this.state.videos} />
+            </div>
+            );
+    }
 }
+
 
 
 
