53b895e39da7525c48dd51e5630eda03aed9dda5
Khalefa <khalefa@addrek.com>
2017-11-12 13:50:46 +0300

    hello

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyBF0ZO7Ut3T_GqemzqZmbBiGLj5vw9_Lbk';
+
+
+YTSearch({key:API_KEY,term: 'surfboards'},function(data){
+    console.log(data);
+})
 
 const App = () => {
     return (
