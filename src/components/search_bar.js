669131f31a2524188694d7bb808efec8788fc379
Khalefa <khalefa@addrek.com>
2017-11-12 13:24:45 +0300

    hello

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,14 @@ import React, {Component} from 'react';
 //const Component = React.Component;
 
 class SearchBar extends Component {
-    render() {
-    return (<input />);
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
     }
+    render() {
+        return (<input onChange={(event) => console.log(event.target.value)} />);
+    }
+
 }
 export default SearchBar;
