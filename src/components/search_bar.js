4ececdbeb1902b661ff191e3cd34dbd7c613c0ec
Khalefa <khalefa@addrek.com>
2017-11-12 13:14:01 +0300

    hello

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+//or we can do it as this:
+//const Component = React.Component;
+
+class SearchBar extends Component {
+    render() {
+    return (<input />);
+    }
+}
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyBF0ZO7Ut3T_GqemzqZmbBiGLj5vw9_Lbk';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+    Hi!
+    <SearchBar />
+    </div>
+    );
 }
+
+
 
 ReactDom.render(<App />,document.querySelector(".container"));
