1efe42c7af13981dd5aa5421367fa732f350f96e
Khalefa <khalefa@addrek.com>
2017-11-12 12:55:56 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const API_KEY = 'AIzaSyBF0ZO7Ut3T_GqemzqZmbBiGLj5vw9_Lbk';
+
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+ReactDom.render(<App />,document.querySelector(".container"));
